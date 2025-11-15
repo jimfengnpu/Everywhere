01a7fdea7dd3e20e37949ba0ae799dcb2b1f074b
Dear.Va <dear.va.love@gmail.com>
2025-11-15 19:24:00 +0800

    perf: update EFCore to 10.0.0

--- src/Everywhere/Database/Migrations/20251115112206_Update_10_0_0.Designer.cs
+++ src/Everywhere/Database/Migrations/20251115112206_Update_10_0_0.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Everywhere.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Everywhere.Database.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251115112206_Update_10_0_0")]
+    partial class Update_10_0_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
